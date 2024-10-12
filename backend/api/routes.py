5b899c81fb525421c352c0f4d20612f2ad84a505
zxc228 <ilya423@yandex.ru>
2024-10-12 16:29:54 +0200

    half admin

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, Response, jsonify
-from backend.models import Post, Vacancy, Employee
+from backend.models import Post, Documents, Partners, Project
 import json
 
 api = Blueprint('api', __name__)
@@ -13,7 +13,8 @@ def get_news():
             "id": news.id,
             "date": news.date.strftime('%Y-%m-%d'), 
             "title": news.title,
-            "content": news.content
+            "content": news.content,
+            "category": news.category
         }
         for news in news_list
     ]
@@ -35,7 +36,8 @@ def get_one_news(id):
         "id": one_news.id,
         "date": one_news.date.strftime('%Y-%m-%d'), 
         "title": one_news.title,
-        "content": one_news.content
+        "content": one_news.content,
+        "category": one_news.category
     }
     response = Response(
         response=json.dumps(one_news_data, ensure_ascii=False),
@@ -46,99 +48,3 @@ def get_one_news(id):
 
 
 
-@api.route("/api/vacancies", methods=['GET'])
-def get_vacancies():
-    vacancy_list = Vacancy.query.all()
-    vacancy_data = [
-        {
-            "id": vacancy.id,
-            "date": vacancy.date.strftime('%Y-%m-%d'),
-            "title": vacancy.title,
-            "content": vacancy.content,
-            "salary": vacancy.salary,
-            "status": "Active" if vacancy.status else "Inactive"
-        }
-        for vacancy in vacancy_list
-    ]
-    response = Response(
-        response=json.dumps(vacancy_data, ensure_ascii=False),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-
-@api.route("/api/vacancies/<int:id>", methods=['GET'])
-def get_one_vacancy(id):
-    one_vacancy = Vacancy.query.get(id)
-    if not one_vacancy:
-        return Response(
-            response=json.dumps({"message": "Vacancy not found"}, ensure_ascii=False),
-            status=404,
-            mimetype='application/json'
-        )
-
-    one_vacancy_data = {
-        "id": one_vacancy.id,
-        "date": one_vacancy.date.strftime('%Y-%m-%d'),
-        "title": one_vacancy.title,
-        "content": one_vacancy.content,
-        "salary": one_vacancy.salary,
-        "status": "Active" if one_vacancy.status else "Inactive"
-    }
-    response = Response(
-        response=json.dumps(one_vacancy_data, ensure_ascii=False),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-
-@api.route("/api/employees", methods=['GET'])
-def get_employees():
-    employees_list = Employee.query.all()
-    employees_data = [
-        {
-            "id": employee.id,
-            "name": employee.name,
-            "surname": employee.surname,
-            "patronimyc": employee.patronymic,
-            "vacancy": employee.vacancy,
-            "photo": employee.photo
-        }
-        for employee in employees_list
-    ]
-    response = Response(
-        response=json.dumps(employees_data, ensure_ascii=False),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-
-@api.route("/api/employee/<int:id>", methods=['GET'])
-def get_one_employee(id):
-    one_employee = Employee.query.get(id)
-    if not one_employee:
-        return Response(
-            response=json.dumps({"message": "Employee not found"}, ensure_ascii=False),
-            status=404,
-            mimetype='application/json'
-        )
-
-    one_employee_data = {
-        {
-            "id": one_employee.id,
-            "name": one_employee.name,
-            "surname": one_employee.surname,
-            "patronimyc": one_employee.patronymic,
-            "vacancy": one_employee.vacancy,
-            "photo": one_employee.photo
-        }
-    }
-    response = Response(
-        response=json.dumps(one_employee_data, ensure_ascii=False),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
